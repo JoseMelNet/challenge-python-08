0f6e4ad131474e5690ffc2ae5f44a7007951d879
Jose Melendez <josemelnet@gmail.com>
2020-07-25 16:23:20 -0500

    Decorator Function - Print execution date

--- challenge.py
+++ challenge.py
@@ -1,8 +1,15 @@
 import time
 
 
-def finish_date():
-    # You have to code here!!
+def finish_date(function):
+    def wrapper(*args, **kwargs):
+        import time
+        captured_time = time.gmtime()
+        print_time = time.strftime("Execution time: %d/%m/%Y - %H:%M:%S", captured_time)
+        function(*args, **kwargs)
+        print(print_time)
+        return function(*args, **kwargs)
+    return wrapper
 
 
 @finish_date
